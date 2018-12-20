8c292af1821e18a2525575bcde787128ec8b9539
Marouane Souah <marouanesouah@gmail.com>
2018-12-21 00:18:11 +0100

    Add vue-authentificate

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:8000/";
+
+//if loca storage has token, add it the axios default header
+const token = localStorage.getItem('token');
+
+if (token) {
+    axios.defaults.headers.common = {'Authorization': "bearer " + token};
+}
 
 Vue.config.productionTip = false
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import {VueAuthenticate} from 'vue-authenticate'
 
+Vue.use(VueAxios, axios)
 Vue.use(Vuex)
+
+
+const vueAuth = new VueAuthenticate(Vue.prototype.$http, {
+  baseUrl: 'http://localhost:8000', // Your API domain
+  
+  providers: {
+    live: {
+      clientId: '00970931-21bf-46bc-9be9-f460d43fb6b7',
+      redirectUri: 'http://localhost:8080' ,// Your client app URL
+      authorizationEndpoint : "https://login.microsoftonline.com/common/oauth2/v2.0/authorize/",
+      scope: ["openid","profile","offline_access","User.Read"],
+      scopeDelimiter: ' ',
+      oauthType: '2.0',
+    }
+  }
+})
 
 export default new Vuex.Store({
   state: {
-
+    token: localStorage.getItem('token') || '',
+    isAuthenthicated : false,
+  },
+  getters : {
+    isAuthenticated() {
+      return vueAuth.isAuthenticated()
+    }
   },
   mutations: {
 
